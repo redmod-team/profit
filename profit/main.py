bbba913c2c13045467f5d51ecc01c2ced7584344
Christopher Albert <albert@alumni.tugraz.at>
2019-12-02 20:07:12 +0100

    Surrogate update, benchmarks

--- profit/main.py
+++ profit/main.py
@@ -39,12 +39,13 @@ def quasirand(npoint, ndim, kind='Halton'):
         raise NotImplementedError('Only Halton sequences implemented yet')
 
 def fit(u, y):
-    fresp = gp.GPySurrogate()
-    fresp.train(u.T, y, sigma_n=0.1)
+    fresp = gp.GPFlowSurrogate()
+    fresp.train(u.T.reshape(y.size, -1), y.reshape(y.size, -1))
     def predict(*args):
         return fresp.predict(np.array(args).T)
 
     return predict
+    #return fresp
     
 def read_input(run_dir):
     data = load_txt(os.path.join(run_dir, 'input.txt'))
--- profit/sur/backend/gp.py
+++ profit/sur/backend/gp.py
@@ -8,8 +8,10 @@ import scipy as sp
 from profit.sur import Surrogate
 
 try:
+    import tensorflow as tf
+    import tensorflow_probability as tfp
     import gpflow
-    from gpflow.utilities import print_summary
+    from gpflow.utilities import print_summary, set_trainable, to_default_float
 except:
     pass
 
@@ -139,18 +141,103 @@ class GPFlowSurrogate(Surrogate):
     # TODO
         
     def train(self, x, y, sigma_n=None, sigma_f=1e-6):
+        x = x.reshape(y.size, -1)
+        y = y.reshape(y.size, -1)
+        self.ndim = x.shape[-1]
+        sigma_k = (np.max(y) - np.min(y))**2
+
+        l = np.empty(self.ndim)
+        vary = np.var(y)
+        kern = list()
+        
+        for k in range(self.ndim):
+            l[k] = (np.max(x[:,k]) - np.min(x[:,k]))/2.0
+            kern.append(gpflow.kernels.SquaredExponential(
+                lengthscale=l[k], variance=1.0, active_dims=[k]))
+            set_trainable(kern[k].variance, False)
+            
+        sig1 = gpflow.kernels.Constant()
+        sign = gpflow.kernels.Constant() 
+        term1 = gpflow.kernels.Product([sig1, gpflow.kernels.Sum(kern)])
+        termn = gpflow.kernels.Product([sign] + kern)
+        term1.kernels[0].variance.assign(sigma_k)
+        termn.kernels[0].variance.assign(sigma_k)
+        
+        kerns = termn
+
+        #if(self.ndim == 1):
+        #    kerns = gpflow.kernels.Sum([term1])
+        #else:
+        #    kerns = gpflow.kernels.Sum([term1] + [termn])
+
+        if(self.ndim == 1):
+            self.m = gpflow.models.GPR((x, y), 
+            kernel = kerns,
+            mean_function = gpflow.mean_functions.Linear())
+        else:
+            self.m = gpflow.models.GPR((x, y), 
+            kernel = kerns,
+            mean_function = gpflow.mean_functions.Constant())
+            # TODO: check possible bug in GPFlow why Linear mean doesn't work
+
+        self.m.likelihood.variance.assign(vary)
+
+        print_summary(self.m)
+            
+        # Optimize
+        opt = gpflow.optimizers.Scipy()
+        opt.minimize(self.m.neg_log_marginal_likelihood, 
+           self.m.trainable_variables)
+        #opt = gpflow.optimizers.
+        # opt = tf.optimizers.Adam(0.1)
+        # for i in range(100):
+        #     opt.minimize(self.m.neg_log_marginal_likelihood, 
+        #         var_list=self.m.trainable_variables)
+        #     likelihood = self.m.log_likelihood()
+        #     tf.print(f'GPR with Adam: iteration {i + 1} likelihood {likelihood:.04f}')
+        print_summary(self.m)
+
+        self.sigma = np.sqrt(self.m.likelihood.variance.value())
+        self.trained = True
+
+    def train_single(self, x, y, sigma_n=None, sigma_f=1e-6):
+        x = x.reshape(y.size, -1)
+        y = y.reshape(y.size, -1)
+        self.ndim = x.shape[-1]
+        if x.shape[-1] > 1 :
+            lengthscale = (np.max(np.max(x, -1) - np.min(x, -1)))/2.0
+        else:
+            lengthscale = (np.max(x) - np.min(x))/2.0
+        print(lengthscale)
         self.m = gpflow.models.GPR((x, y), 
-            kernel=gpflow.kernels.SquaredExponential())
-            #mean_function=gpflow.mean_functions.Linear())
-        self.m.kernel.lengthscale.assign(np.max(x)-np.min(x))
-        self.m.kernel.variance.assign((np.max(y)-np.min(y))**2)
-        self.m.likelihood.variance.assign(np.var(y))
+            kernel=gpflow.kernels.SquaredExponential(),
+            mean_function=gpflow.mean_functions.Linear())
+        self.m.kernel.lengthscale.assign(lengthscale)
+        vary = np.var(y)
+        #self.m.kernel.variance.assign(1e2*vary)
+        self.m.kernel.variance.assign((np.max(y) - np.min(y))**2)
+        #set_trainable(self.m.kernel.variance, False)
+        #self.m.kernel.lengthscale.prior = tfp.distributions.Uniform(
+        #    to_default_float(lengthscale/y.size), to_default_float(lengthscale))
+
+        mea = lengthscale/2.0
+        vari = lengthscale/2.0
+        mu = np.log(mea**2/np.sqrt(vari+mea**2))
+        sig = np.sqrt(np.log(1.0 + vari/mea**2))
+        #self.m.kernel.lengthscale.prior = tfp.distributions.LogNormal(
+        #    to_default_float(mu), to_default_float(sig))
+        #set_trainable(self.m.kernel.lengthscale, False)
+        self.m.likelihood.variance.assign(vary)
         print_summary(self.m)
         
         # Optimize
         opt = gpflow.optimizers.Scipy()
         opt.minimize(lambda: -self.m.log_marginal_likelihood(), 
-            self.m.trainable_parameters, method = 'Powell')
+            self.m.trainable_variables)
+        #opt = gpflow.optimizers.
+        #opt = tf.optimizers.Adam()
+        #opt.minimize(lambda: -self.m.log_marginal_likelihood(), 
+        #    var_list=self.m.trainable_variables)
         print_summary(self.m)
 
         self.sigma = np.sqrt(self.m.likelihood.variance.value())
@@ -167,7 +254,7 @@ class GPFlowSurrogate(Surrogate):
         if not self.trained:
             raise RuntimeError('Need to train() before predict()')
             
-        return self.m.predict_y(x)
+        return self.m.predict_y(x.reshape(-1, self.ndim))
 
 class GPySurrogate(Surrogate):
     def __init__(self):
@@ -177,14 +264,24 @@ class GPySurrogate(Surrogate):
     # TODO
         
     def train(self, x, y, sigma_n=None, sigma_f=1e-6):
+        print(x.shape)
+        if x.shape[1] > 1 :
+            lengthscale = np.max(np.max(x,1)-np.min(x,1))
+        else:
+            lengthscale = np.max(x)-np.min(x)
         kernel = GPy.kern.RBF(input_dim=x.shape[1], 
                               variance=1e2*(np.max(y)-np.min(y))**2, 
-                              lengthscale=np.max(x)-np.min(x))
-        self.m = GPy.models.GPRegression(x, y.reshape([y.size,1]), kernel)
-        self.m.Gaussian_noise.variance = 1e-4*(np.max(y)-np.min(y))**2
-        #print(self.m)
-        self.m.optimize(messages=False)
-        #print(self.m)
+                              lengthscale=lengthscale)
+
+        mf = GPy.mappings.Linear(x.shape[1], 1)
+
+        self.m = GPy.models.GPRegression(x, y.reshape([y.size,1]), kernel,
+                                         mean_function = mf)
+        #self.m.Gaussian_noise.variance = 1e-4*(np.max(y)-np.min(y))**2
+        self.m.Gaussian_noise.variance = np.var(y)
+        print(self.m)
+        self.m.optimize(messages=True)
+        print(self.m)
 
         self.sigma = np.sqrt(self.m.Gaussian_noise.variance.values)
         self.trained = True
