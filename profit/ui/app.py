69f2f75e23fe66c0e318a64ad1960fde125a3d10
Christopher Albert <albert@alumni.tugraz.at>
2020-01-14 12:26:34 +0100

    Dropdowns UI 1D+2D graphs

--- profit/ui/app.py
+++ profit/ui/app.py
@@ -22,8 +22,14 @@ tab_selected_style = {
 }
 
 indata = pd.read_csv('input.txt', delim_whitespace=True, escapechar='#')
+indata.rename(columns=lambda x: x.strip(), inplace=True)
 outdata = pd.read_csv('output.txt', delim_whitespace=True, escapechar='#')
+outdata.rename(columns=lambda x: x.strip(), inplace=True)
+
 data = pd.concat([indata, outdata], 1)
+
+invars = indata.keys()
+dropdown_opts = [{'label': invar, 'value': invar} for invar in invars]
 
 app.layout = html.Div(
     [
@@ -33,9 +39,36 @@ app.layout = html.Div(
                     id='tabs',
                     children=[
                         dcc.Tab(
-                            label='Graphs',
+                            label='1D Graphs',
                             selected_style=tab_selected_style,
-                            children=[html.Div(dcc.Graph(id="graph1"))]
+                            children=[
+                                html.Div(dcc.Dropdown(
+                                    id='invar',
+                                    options=dropdown_opts,
+                                    value=invars[0]
+                                )),
+                                html.Div(dcc.Graph(id='graph1'))
+                            ]
+                        ),
+                        dcc.Tab(
+                            label='2D Graphs',
+                            selected_style=tab_selected_style,
+                            children=[
+                                html.Div([
+                                    dcc.Dropdown(
+                                        id='invar1',
+                                        options=dropdown_opts,
+                                        value=invars[0]
+                                    ),
+                                    dcc.Dropdown(
+                                        id='invar2',
+                                        options=dropdown_opts,
+                                        value=invars[1]
+                                    ),
+                                ]
+                                ),
+                                html.Div(dcc.Graph(id='graph2'))
+                            ]
                         ),
                         dcc.Tab(
                             label='Tables',
@@ -59,15 +92,32 @@ app.layout = html.Div(
 
 
 @app.callback(
-    dash.dependencies.Output("graph1", "figure"),
-    [dash.dependencies.Input("tabs", "value")]
+    dash.dependencies.Output('graph1', 'figure'),
+    [dash.dependencies.Input('tabs', 'value'),
+     dash.dependencies.Input('invar', 'value'), ]
 )
-def update_figure(selected):
+def update_figure(tab, invar):
+    if invar is None:
+        return go.Figure()
     fig = go.Figure(data=[
-        # go.Scatter(x=indata.values[:, 0],
-        #           y=outdata.values[:, 0], mode='markers'),
-        go.Scatter3d(x=indata.values[:, 0],
-                     y=indata.values[:, 1],
+        go.Scatter(x=indata[invar].values,
+                   y=outdata.values[:, 0], mode='markers')
+    ])
+    return fig
+
+
+@app.callback(
+    dash.dependencies.Output('graph2', 'figure'),
+    [dash.dependencies.Input('tabs', 'value'),
+     dash.dependencies.Input('invar1', 'value'),
+     dash.dependencies.Input('invar2', 'value'), ]
+)
+def update_figure2(tab, invar1, invar2):
+    if invar1 is None or invar2 is None:
+        return go.Figure()
+    fig = go.Figure(data=[
+        go.Scatter3d(x=indata[invar1],
+                     y=indata[invar2],
                      z=outdata.values[:, 0], mode='markers')
     ])
     return fig
