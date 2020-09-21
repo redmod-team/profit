9cb550af49aa27a7c74586793ff9f252444e2693
Christopher Albert <albert@alumni.tugraz.at>
2020-09-21 20:25:30 +0200

    Fortran kernels

--- profit/sur/backend/gpfunc.f90
+++ profit/sur/backend/gpfunc.f90
@@ -5,19 +5,22 @@ implicit none
 contains
 
 ! Build kernel matrix
-subroutine build_K(xa, xb, hyp, K, kern)
-  real(8), intent(in)    :: xa(:, :), xb(:, :), hyp(:)  ! Points and hyperparams
-  real(8), intent(inout) :: K(:, :)  ! Result matrix
+subroutine build_K(np, ndim, xa, xb, K, kern)
+  integer, intent(in)    :: np, ndim
+  real(8), intent(in)    :: xa(np, ndim), xb(np, ndim) ! Points and hyperparams
+  real(8), intent(inout) :: K(np, np)  ! Result matrix
   external :: kern  ! Kernel function
   real(8) :: kern   ! Have to split these in two lines for f2py
 
-  integer :: ka, kb, nhyp
-
-  nhyp = size(hyp)
+  real(8) :: xdiff(np, ndim)
+  integer :: ka, kb
 
   do kb = 1, size(xb, 1)
+     do ka = 1, np
+      xdiff(ka, :) = xa(ka, :) - xb(kb, :)
+    end do
     do ka = 1, size(xa, 1)
-      K(ka, kb) = hyp(nhyp)*kern(xa(ka, :) - xb(kb, :), hyp(:size(xa, 2)))
+      K(ka, kb) = kern(xdiff(ka, :))
     end do
   end do
 
