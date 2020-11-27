eab0da80c79e3f89a3c63cfd370da6bc79cfb457
Christopher Albert <albert@alumni.tugraz.at>
2020-11-27 14:41:55 +0100

    Update GP functions F90

--- profit/sur/backend/gpfunc.f90
+++ profit/sur/backend/gpfunc.f90
@@ -15,28 +15,51 @@ subroutine build_K(xa, xb, hyp, K, kern)
 
   nhyp = size(hyp)
 
-  do kb = 1, size(xb)
-    do ka = 1, size(xa)
-      K(ka, kb) = hyp(nhyp)*kern(xa(:, ka) - xb(:, kb), hyp(:size(xa, 1)))
+  do kb = 1, size(xb, 1)
+    do ka = 1, size(xa, 1)
+      K(ka, kb) = hyp(nhyp)*kern(xa(ka, :) - xb(kb, :), hyp(:size(xa, 2)))
     end do
   end do
 
 end subroutine build_K
 
-function kern_sqexp(xa, xb, hyp)
-  real(8) :: kern_sqexp
-  real(8), intent(in) :: xa(:), xb(:), hyp(:)
-  kern_sqexp = exp(-0.5d0*(sum(xb-xa)/hyp(1))**2)
-end function kern_sqexp
 
-subroutine build_K_sqexp(np, ndim, nhyp, xa, xb, hyp, K)
-  integer, intent(in)    :: np, ndim, nhyp
-  real(8), intent(in)    :: xa(ndim, np)
-  real(8), intent(in)    :: xb(ndim, np)
-  real(8), intent(in)    :: hyp(nhyp)     ! Points and hyperparams
+subroutine build_K_sqexp(np, ndim, xa, xb, l, K)
+  integer, intent(in)    :: np, ndim
+  real(8), intent(in)    :: xa(np, ndim)
+  real(8), intent(in)    :: xb(np, ndim)
+  real(8), intent(in)    :: l(ndim)       ! Length scales
   real(8), intent(inout) :: K(np, np)     ! Result matrix
 
-  call build_K(xa, xb, hyp, K, kern_sqexp)
+  real(8) :: xdiff(np, ndim)
+  integer :: ka, kb
+
+  do kb = 1, np
+    xdiff = (xa(:, :) - xb(kb, :)) / (2.0d0*l)
+    do ka = 1, np
+      K(ka, kb) = exp(-sum(xdiff(ka, :)**2))
+    end do
+  end do
 end subroutine build_K_sqexp
+
+
+subroutine build_dKdl_sqexp(np, ndim, xa, xb, l, K)
+  integer, intent(in)    :: np, ndim
+  real(8), intent(in)    :: xa(np, ndim)
+  real(8), intent(in)    :: xb(np, ndim)
+  real(8), intent(in)    :: l(ndim)       ! Length scales
+  real(8), intent(inout) :: K(np, np)     ! Result matrix
+
+  real(8) :: xdiff(np, ndim)
+  integer :: ka, kb
+
+  do kb = 1, np
+    xdiff = (xa(:, :) - xb(kb, :)) / (2.0d0*l)
+    do ka = 1, np
+      K(ka, kb) = exp(-sum(xdiff(ka, :)**2))
+    end do
+  end do
+end subroutine build_dKdl_sqexp
+
 
 end module gpfunc
