3b995cc3aa6da65ade64921f21d21d694983ab3f
Christopher Albert <albert@alumni.tugraz.at>
2020-11-30 17:11:29 +0100

    Fortran build_K finished

--- draft/generate_kern_vec/bench_inline.f90
+++ draft/generate_kern_vec/bench_inline.f90
@@ -1,22 +1,27 @@
 program bench_inline
-use gpfunc, only: build_K
+use gpfunc, only: build_K, kern_sqexp
 implicit none
 
 integer, parameter :: nx = 1024
-integer, parameter :: nd = 10
+integer, parameter :: nd = 4
 
-real(8), allocatable :: x(:, :)
+real(8), allocatable :: x(:, :), y(:), l(:)
 real(8), allocatable :: K(:, :)
 integer :: kx, kd
 integer(8) :: count, count_rate, count_max
 real(8) :: tic, toc
 
+integer :: info
 
-external :: kern_sqexp
+external :: dpotrf
+external :: dtrmv
 
-allocate(K(nx, nx), x(nd, nx))
+allocate(K(nx, nx), x(nd, nx), l(nd), y(nx))
+
+l = 1d0
 
 do kx = 1, nx
+    call random_number(y(kx))
     do kd = 1, nd
         call random_number(x(kd, kx))
     end do
@@ -28,13 +33,36 @@ K = 0d0
 call system_clock(count, count_rate, count_max)
 tic = count*1d3/count_rate
 
-call build_K(nd, nx, nx, x, x, K, kern_sqexp)
+call build_K(nd, nx, nx, x, x, K, kern_sqexp, l, 1d0, 1d-12)
 
 call system_clock(count, count_rate, count_max)
 toc = count*1d3/count_rate
 print *, 'Time build_K_vec:', toc - tic, 'ms'
 print *, K(5,5), K(nx,nx-5)
 print *, ''
+
+
+call system_clock(count, count_rate, count_max)
+tic = count*1d3/count_rate
+
+call dpotrf('L', nx, K, nx, info)
+
+call system_clock(count, count_rate, count_max)
+toc = count*1d3/count_rate
+print *, 'Result Cholesky:', info
+print *, 'Time Cholesky:', toc - tic, 'ms'
+print *, ''
+
+call system_clock(count, count_rate, count_max)
+tic = count*1d3/count_rate
+
+call dtrmv('L', 'N', 'N', nx, K, nx, y, 1)
+
+call system_clock(count, count_rate, count_max)
+toc = count*1d3/count_rate
+print *, 'Time matmul:', toc - tic, 'ms'
+
+deallocate(K, x, y, l)
 
 
 end program bench_inline
--- draft/generate_kern_vec/gpfunc_bench.f90
+++ draft/generate_kern_vec/gpfunc_bench.f90
@@ -3,24 +3,49 @@ implicit none
 
 contains
 
-! Build kernel matrix
-subroutine build_K(nd, nxa, nxb, xa, xb, K, kern)
+subroutine build_K(nd, nxa, nxb, xa, xb, K, kern, l, sig2f, sig2n, periodic)
+    ! Build a kernel matrix using a function `kern` to construct columns/rows
     integer, intent(in)    :: nd, nxa, nxb  ! Dimension and number of points
     real(8), intent(in)    :: xa(nd, nxa), xb(nd, nxb) ! Points
-    real(8), intent(inout) :: K(nxa, nxb)  ! Result matrix
-    external :: kern  ! Kernel function
+    real(8), intent(inout) :: K(nxa, nxb)   ! Output: kernel matrix
+    real(8), intent(in)    :: l(nd)         ! Length scales
+    real(8), intent(in)    :: sig2f         ! Output scale covariance
+    real(8), intent(in)    :: sig2n         ! Noise covariance
+    logical, optional      :: periodic(nd)  ! `True` for periodic directions
+    external :: kern  ! Kernel function `kern(nd, nx, xa, xb, out)`
 
-    integer :: ka
+    integer :: ka, kd
+    real(8) :: xanorm(nd, nxa), xbnorm(nd, nxb)
 
+    if (present(periodic)) then
+        xbnorm = 0d0
+        !$omp simd
+        do kd = 1, nd
+            if (periodic(kd)) then
+                xanorm(kd, :) = (xa(kd, :) - xb(kd, :))/l(kd)
+            else
+                xanorm(kd, :) = sin(xa(kd, :) - xb(kd, :))/l(kd)
+            endif
+        end do
+    else
+        !$omp simd
+        do kd = 1, nd
+            xanorm(kd, :) = xa(kd, :)/l(kd)
+            xbnorm(kd, :) = xb(kd, :)/l(kd)
+        end do
+    endif
     !$omp parallel do
     do ka = 1, nxa
-        call kern(nd, nxb, xa(:, ka), xb, K(:, ka))
+        call kern(nd, nxb, xanorm(:, ka), xbnorm, K(:, ka))
+        K(:, ka) = sig2f*K(:, ka)
+        K(ka, ka) = K(ka, ka) + sig2n
     end do
+    !$omp end parallel do
 
 end subroutine build_K
 
+
 subroutine kern_sqexp(nd, nx, xa, xb, out)
-    implicit none
     integer, intent(in) :: nd, nx
     real(8), intent(in) :: xa(nd), xb(nd, nx)
     real(8), intent(out) :: out(nx)
