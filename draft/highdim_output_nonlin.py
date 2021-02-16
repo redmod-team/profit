0a144ec15fa31018e2d0cc22ac8b5c538fdfa0f8
Christopher Albert <albert@alumni.tugraz.at>
2021-02-16 17:56:05 +0100

    Checking scipy optimizer with PyTorch

--- draft/highdim_output_nonlin.py
+++ draft/highdim_output_nonlin.py
@@ -1,94 +1,7 @@
-"""High-dimensional output
-
-This script drafts the following use-case: we make a parameter study over some
-input parameters x and the domain code yields an output vector y with many
-parameters. This is typically the case, when function-valued or even
-"pixel-valued" output is produced, like on a 2D map (like 1024x1024).
-Before fitting an input-output relation, one has to reduce the output to
-a manageable number of dimensions. This means extracting the relevant
-features.
-
-Imagine the following idea: instead of viewing the output as many 1D outputs,
-we look at it as a single vector in a high-dimensional space. Linear
-combinations between vectors give new vectors. We can choose y from our
-training data to span a basis in this vector space.
-
-"""
-#%%
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.sparse.linalg.eigen import eigsh
-
-def f(u, x):
-    return np.cos(u*x)
-
-x = np.linspace(0, 20, 100)
-
-ntrain = 50
-
-utrain = np.random.rand(ntrain)
-plt.figure()
-
-ytrain = []
-for u in utrain:
-    ytrain.append(f(u, x))
-    plt.plot(x, ytrain[-1])
-plt.xlabel('Independent variable x')
-plt.ylabel(f'Output f(x;u) for different u')
-ytrain = np.array(ytrain)
-
-"""
-Now we want to write
-y_4 = a_1 y_1 + a_2 y_2 + a_3 y_3
-
-This can be done by projection with inner products:
-
-y_1*y_4 = a_1 y_1*y_1 + a_2 y_1*y_2 + a_3 y_1*y_3
-y_2*y_4 = a_1 y_2*y_1 + a_2 y_2*y_2 + a_3 y_2*y_3
-y_3*y_4 = a_1 y_3*y_1 + a_2 y_3*y_2 + a_3 y_3*y_3
-
-We see that we have to solve a linear system with the
-covariance matrix M_ij = y_i*y_j . To find the most relevant features
-and reduce dimensionality, we use a PCA with only the highest eigenvalues.
-"""
-M = np.empty((ntrain, ntrain))  # Covariance matrix
-for i in range(ntrain):
-    for j in range(ntrain):
-        M[i,j] = ytrain[i].dot(ytrain[j])
-
-w, Q = eigsh(M, 10)
-plt.figure()
-plt.plot(w)
-
-plt.figure()
-for i in range(10):
-    plt.plot(w[i]*Q[:,i].dot(ytrain))
-plt.title('10 most significant eigenvectors')
-plt.xlabel('Independent variable x')
-plt.ylabel('Eigenvectors scaled by eigenvalue')
-#%% Testing
-
-utest = 0.7
-ytest = f(utest, x)
-b = np.empty(ntrain)
-for i in range(ntrain):
-    b[i] = ytest.dot(ytrain[i])
-
-a = Q.dot(np.diag(1.0/w).dot(Q.T.dot(b)))
-#a = np.linalg.solve(M, b)
-
-plt.figure()
-plt.plot(a.dot(ytrain), '--')
-plt.plot(ytest)
-plt.xlabel('Independent variable x')
-plt.ylabel(f'Output f(x;u={utest})')
-plt.legend(['Reconstruction', 'Reference'])
-
 # %%
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
 
 class Autoencoder(nn.Module):
     def __init__(self, D):
@@ -101,7 +14,7 @@ class Autoencoder(nn.Module):
         x = F.linear(x, self.W.t())
         return x
 
-D = 6
+D = 2
 x = torch.rand(D)
 
 ae = Autoencoder(D)
@@ -110,36 +23,55 @@ y = ae.forward(x)
 print(x)
 print(y)
 # %%
-ntrain = 20
-input = torch.randn(ntrain, D)
+ntrain = 100
+input = torch.rand(ntrain, D) - 0.5
 output = ae(input)
 target = input
 criterion = nn.MSELoss()
 loss = criterion(output, target)
 
 print(loss)
-#%%
-import torch.optim as optim
+#%% Torch LBFGS
+# import torch.optim as optim
 
-# create your optimizer
-optimizer = optim.LBFGS(ae.parameters())
+# # create your optimizer
+# optimizer = optim.LBFGS(ae.parameters())
 
-for inp in input:
-    def closure():
-        if torch.is_grad_enabled():
-            optimizer.zero_grad()
-        output = ae(inp)
-        loss = criterion(output, inp)
-        if loss.requires_grad:
-            loss.backward()
-        return loss
-    optimizer.step(closure)
+# for inp in input:
+#     def closure():
+#         if torch.is_grad_enabled():
+#             optimizer.zero_grad()
+#         output = ae(inp)
+#         loss = criterion(output, inp)
+#         if loss.requires_grad:
+#             loss.backward()
+#         return loss
+#     optimizer.step(closure)
 
-output = ae(input)
-print(criterion(output, input))
+# output = ae(input)
+# print(criterion(output, input))
 
 # %%
 
 print(ae(input)-input)
+
+# %%
+from scipy.optimize import minimize
+
+def target(w):
+    with torch.no_grad():
+        ae.W.copy_(torch.Tensor(w.reshape([D,D])))
+    if ae.W.grad is not None:
+        ae.W.grad.zero_()
+    output = ae(input)
+    loss = criterion(output, input)
+    if loss.requires_grad:
+        loss.backward()
+
+    return loss.detach().flatten().numpy(), ae.W.grad.detach().flatten().numpy()
+
+target(ae.W)
+# %%
+minimize(target, ae.W.detach().flatten(), method='Powell', jac=True)
 
 # %%
