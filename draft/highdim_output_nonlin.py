c7c7c114b7bb9926dd4c4780341901c4e8cb4c16
Christopher Albert <albert@alumni.tugraz.at>
2021-02-17 17:27:10 +0100

    PyTorch autoencoder stuff

--- draft/highdim_output_nonlin.py
+++ draft/highdim_output_nonlin.py
@@ -1,38 +1,85 @@
-# %%
+# %% See https://pytorch.org/tutorials/beginner/blitz/neural_networks_tutorial.html
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
 
-class Autoencoder(nn.Module):
-    def __init__(self, D):
-        super(Autoencoder, self).__init__()
-        self.W = nn.Parameter(torch.randn((D,D))/D)
+class LinearAutoencoder(nn.Module):
+    """Linear autoencoder as 'rich man's PCA'.
+
+    Weights of the encoder are tied to the decoder
+    to have the symmetric structure x = W.T @ W @ x .
+    """
+    def __init__(self, D, d):
+        super(LinearAutoencoder, self).__init__()
+        self.W = nn.Parameter(torch.randn((d,D))/np.sqrt(d*D))
         #self.W = nn.Parameter(torch.eye(D))
 
     def forward(self, x):
-        x = F.linear(x, self.W)
-        x = F.linear(x, self.W.t())
+        x = F.linear(x, self.W)     # encoder
+        x = F.linear(x, self.W.t()) # decoder
         return x
 
-D = 2
-x = torch.rand(D)
+class Autoencoder(nn.Module):
+    """Nonlinear autoencoder wich activation functions
 
-ae = Autoencoder(D)
+    TODO: Add hidden layers and check if symmetry is needed
+    """
+    def __init__(self, D, d):
+        super(Autoencoder, self).__init__()
+        self.W = nn.Parameter(torch.randn((d,D))/np.sqrt(d*D))
+        #self.W = nn.Parameter(torch.eye(D))
+
+    def forward(self, x):
+        x = F.tanh(F.linear(x, self.W))
+        x = F.tanh(F.linear(x, self.W.t()))
+        return x
+
+D = 4
+d = 2
+
+Wlift = torch.Tensor([[1.0, 0], [0, 1.0], [0.0, 0.0], [0.0, 0.0]])
+
+x = Wlift @ torch.rand(d)
+
+ae = LinearAutoencoder(D, d)
 print(list(ae.parameters()))
 y = ae.forward(x)
 print(x)
 print(y)
 # %%
 ntrain = 100
-input = torch.rand(ntrain, D) - 0.5
+input = (Wlift @ (torch.rand(d, ntrain) - 0.5)).T
 output = ae(input)
 target = input
 criterion = nn.MSELoss()
 loss = criterion(output, target)
 
 print(loss)
-#%% Torch LBFGS
-# import torch.optim as optim
+#%% PyTorch custom plain gradient descent: quite slow
+# learning_rate = 0.1
+# for k in range(100):
+#     ae.zero_grad()
+#     output = ae(input)
+#     loss = criterion(output, target)
+#     loss.backward()
+#     for f in ae.parameters():
+#         f.data.sub_(f.grad.data * learning_rate)
+#     print(loss.data)
+
+#%% PyTorch SGD variant (Adam)
+optimizer = optim.Adam(ae.parameters(), lr=0.01)
+
+for k in range(100):
+    optimizer.zero_grad()
+    output = ae(input)
+    loss = criterion(output, target)
+    loss.backward()
+    optimizer.step()
+    print(loss.data)
+
+#%% PyTorch Quasi-Newton (LBFGS)
 
 # # create your optimizer
 # optimizer = optim.LBFGS(ae.parameters())
@@ -51,27 +98,34 @@ print(loss)
 # output = ae(input)
 # print(criterion(output, input))
 
+#%% PyTorch SGD variant (Adam)
+
+
+#%% SciPy Quasi-Newton (Powell)
+# from scipy.optimize import minimize
+
+# def target(w):
+#     with torch.no_grad():
+#         ae.W.copy_(torch.Tensor(w.reshape([d,D])))
+#     if ae.W.grad is not None:
+#         ae.W.grad.zero_()
+#     output = ae(input)
+#     loss = criterion(output, input)
+#     if loss.requires_grad:
+#         loss.backward()
+#     with torch.no_grad():
+#         print(loss.data)
+
+#     return loss.detach().numpy(), ae.W.grad.detach().flatten().numpy()
+
+# target(ae.W)
+# res = minimize(target, ae.W.detach().flatten(), method='Powell', jac=True)
+# print(res.fun)
+# with torch.no_grad():
+#     ae.W.copy_(torch.Tensor(res.x.reshape([d,D])))
 # %%
-
-print(ae(input)-input)
-
-# %%
-from scipy.optimize import minimize
-
-def target(w):
-    with torch.no_grad():
-        ae.W.copy_(torch.Tensor(w.reshape([D,D])))
-    if ae.W.grad is not None:
-        ae.W.grad.zero_()
-    output = ae(input)
-    loss = criterion(output, input)
-    if loss.requires_grad:
-        loss.backward()
-
-    return loss.detach().flatten().numpy(), ae.W.grad.detach().flatten().numpy()
-
-target(ae.W)
-# %%
-minimize(target, ae.W.detach().flatten(), method='Powell', jac=True)
-
+print()
+print("Weights: ")
+print(ae.W @ ae.W.T)
+print(ae.W.T @ ae.W)
 # %%
