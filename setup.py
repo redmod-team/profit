c571441706829fc2bf81d257a12464bf7617b9b1
Robert Babin <profit.rykath@xoxy.net>
2021-02-19 17:48:55 +0100

    Update setup - fix readthedocs, optional requirements
    Try fixing readthedocs generation
     - Detect compilation on readthedocs -> disable fortran
     - additional dependencies
    Add optional dependencies `docs` and `gpu`
    Add installation flag `--no-fortran`
    Some cleaning
    Update README.md
     - documentation status (badge)
     - installation documentation

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 # setup.py
 # install script using setuptools / numpy.distutils
-# TODO: should be integrated in setup.cfg
 
-import sys, site
-site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
+import sys
+import os
+import site
 
 from numpy.distutils.core import Extension, setup
 
@@ -12,18 +12,27 @@ ext_kwargs = {
     'extra_f90_compile_args': ['-Wall', '-march=native', '-O2', '-fopenmp', 
                                '-g', '-fbacktrace']} 
 
-
 ext_gpfunc = Extension(
-    name = 'profit.sur.backend.gpfunc', 
-    sources = ['profit/sur/backend/gpfunc.f90'],
+    name='profit.sur.backend.gpfunc',
+    sources=['profit/sur/backend/gpfunc.f90'],
     **ext_kwargs)
 
 ext_kernels = Extension(
-    name = 'profit.sur.backend.kernels',
-    sources = ['profit/sur/backend/kernels.f90', 'profit/sur/backend/kernels_base.f90'],
+    name='profit.sur.backend.kernels',
+    sources=['profit/sur/backend/kernels.f90', 'profit/sur/backend/kernels_base.f90'],
     **ext_kwargs)
 
 
 if __name__ == "__main__":
-    setup(ext_modules = [ext_gpfunc, ext_kernels])
+    # explicitly allow installation in user site in development mode
+    site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
+
+    # forgo compiling fortran while building the docs for readthedocs
+    if os.environ.get('READTHEDOCS') == 'True':
+        setup()
+    elif "--no-fortran" in sys.argv[1:]:
+        sys.argv.remove("--no-fortran") # need to remove argument before setup() is called
+        setup()
+    else:
+        setup(ext_modules=[ext_gpfunc, ext_kernels])
 
