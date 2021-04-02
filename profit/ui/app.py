c18f4d377e884139ecd9caf2537df15ca3191da9
Michael Hadwiger <m.hadwiger@student.tugraz.at>
2021-04-02 11:27:15 +0200

    small cleanup

--- profit/ui/app.py
+++ profit/ui/app.py
@@ -1,6 +1,5 @@
 import dash
 import dash_table
-import pandas as pd
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
@@ -178,21 +177,6 @@ def init_app(config):
                 # ) # TODO: fix table
             ])
         ]),
-        # html.Div(['Specify range with slider or via text input (min/max or center/range) - #digits',
-        #     dcc.Input(id='num-digits', type='number', min=0, value=3),
-        #     html.Button(id='reset-button', children='reset slider', n_clicks=0)
-        # ]),
-        # html.Div(children=[
-        #     html.B('Range min/max: '),
-        #     html.I('Data-range: ('), html.I(id='graph1_min'), ' - ', html.I(id='graph1_max'),
-        #     html.I(')'),
-        #     html.Br(),
-        #     dcc.Input(id='range-min', type='number', placeholder='range min', step=0.001),
-        #     dcc.Input(id='range-max', type='number', placeholder='range max', step=0.001),
-        #     html.B('  center & span:'),
-        #     dcc.Input(id='center', type='number', placeholder='center', step=0.001),
-        #     dcc.Input(id='span', type='number', placeholder='span', step=0.001),
-        # ]),
     ])
 
 
@@ -524,52 +508,4 @@ def init_app(config):
         else:
             return {'visibility': 'hidden'}
 
-
-    # @app.callback(
-    #     Output('invar-2-div', 'style'),
-    #     Input('graph-type', 'value')
-    # )
-    # def update_dropdown_visability(graph_type):
-    #     if graph_type == '1D':
-    #         return {'visibility': 'hidden'}
-    #     else:
-    #         return {'visibility': 'visible'}
-
-
-    # @app.callback(
-    #     [Output({'type': 'param-slide', 'index': MATCH}, 'value'), ],
-    #     [Input('graph1', 'relayoutData'), ],
-    #     [State('invar', 'value'), ]
-    # )
-    # def update_range(relayoutdata, invar):
-    #     ind = invars.to_list().index(invar)
-    #     ctx = dash.callback_context
-    #     trigger_id = ctx.triggered[0]["prop_id"].split(".")[0]
-    #     print(trigger_id)
-    #     # print(trigger_id)
-    #     range_list = [None, None]  # set default
-    #     # if trigger_id == 'reset-button':
-    #     #     center = None
-    #     #     span = None
-    #     # elif (trigger_id == 'center' or trigger_id == 'span') and (center is not None and span is not None):
-    #     #     range_list = [center - span, center + span]
-    #     if trigger_id == 'graph1':
-    #         if list(relayoutdata.keys())[0] == 'xaxis.range':
-    #             range_list = relayoutdata['xaxis.range']
-    #         elif list(relayoutdata.keys())[0] == 'xaxis.range[0]':
-    #             range_list[0] = relayoutdata['xaxis.range[0]']
-    #             range_list[1] = relayoutdata['xaxis.range[1]']
-    #     # else:
-    #     #     range_list = [range_min, range_max]
-    #     print(range_list)
-    #     if range_list[0] is not None and range_list[1] is not None:
-    #         span = (range_list[1] - range_list[0]) / 2
-    #         center = range_list[0] + span
-    #         # round values to num_digits
-    #         range_list[0] = round(range_list[0], num_digits)
-    #         range_list[1] = round(range_list[1], num_digits)
-    #         center = round(center, num_digits)
-    #         span = round(span, num_digits)
-    #     step_size = 10 ** -num_digits  # stepsize for input-forms
-    #     return range_list[0], range_list[1], center, span, step_size, step_size, step_size, step_size
     return app
