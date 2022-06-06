80dfd57dd1f08b8c9108367a0d212c3b6ca456b8
Squadula <francesco.kramp@student.tugraz.at>
2022-06-06 19:20:15 +0200

    restructure LinearRegression and ChaospyLinReg class

--- profit/sur/linreg/chaospy_linreg.py
+++ profit/sur/linreg/chaospy_linreg.py
@@ -1,40 +1,83 @@
 import chaospy
 import numpy as np
+
 from profit.sur import Surrogate
 from profit.sur.linreg import LinearRegression
+from profit.defaults import fit_linear_regression as defaults, fit as base_defaults
 
 @Surrogate.register('ChaospyLinReg')
 class ChaospyLinReg(LinearRegression):
+    """Linear regression surrogate using polynomial expansions as basis
+    functions from chaospy https://chaospy.readthedocs.io/en/master/reference/polynomial/index.html
 
-    def __init__(self, model='monomial', order=None, **kwargs):
+    Attributes:
+        # ToDo
+    """
+
+    def __init__(self):
         super().__init__()
-        self.Mdim = order
-        self.model_params = kwargs
+        self.model = None
+        self.model_kwargs = {}
+        self.n_features = None
+
+    def set_model(self, model, model_kwargs):
+        """Sets model parameters for surrogate
+
+        Parameters:
+            model (str): Name of chaospy model to use
+            model_kwargs (dict): Keyword arguments for the model
+        """
         if model == 'monomial':
             self.model = chaospy.monomial
+            if 'order' in model_kwargs:
+                model_kwargs['start'] = 0
+                model_kwargs['stop'] = model_kwargs['order']
+                model_kwargs.pop('order')
+            model_kwargs['dimensions'] = self.ndim
         else:
             self.model = getattr(chaospy.expansion, model)
 
-    def transform(self, X):
-        vars = ['q' + str(i) for i in range(self.ndim)]
-        Phi = self.model(self.Mdim, **self.model_params)
-        if self.ndim > 0:
-            for var in vars[1:]:
-                poly = self.model(self.Mdim, **self.model_params)
-                poly.names = (var,)
-                Phi = chaospy.outer(Phi, poly)
-        return Phi.flatten()(*X.T)
+        self.model_kwargs = model_kwargs
 
-    def train(self, X, y, sigma_n=0.02, sigma_p=0.5):
-        self.pre_train(X, y)
+    def transform(self, X):
+        """Transforms input data on selected basis functions
+
+        Parameters:
+            X (ndarray): Input points
+
+        Returns:
+            Phi (ndarray): Basis functions evaluated at X
+        """
+        if self.model == 'monomial':
+            Phi = self.model(**self.model_kwargs)
+        # if model == 'lagrange':
+        #     # ToDo
+        #     pass
+        else:
+            # ToDo: if isattr?
+            vars = ['q' + str(i) for i in range(self.ndim)]
+            Phi = self.model(**self.model_kwargs)
+            if self.ndim > 0:
+                for var in vars[1:]:
+                    poly = self.model(**self.model_kwargs)
+                    poly.names = (var,)
+                    Phi = chaospy.outer(Phi, poly)
+            Phi = Phi.flatten()
+        self.n_features = len(Phi)
+        return Phi(*X.T)
+
+    def train(self, X, y, model=defaults['model'], model_kwargs=defaults['model_kwargs'],
+              sigma_n=defaults['sigma_n'], sigma_p=defaults['sigma_p']):
+        self.pre_train(X, y, sigma_n, sigma_p)
+        self.set_model(model, model_kwargs)
+
         Phi = self.transform(self.Xtrain)
-        print(self.Mdim, self.ndim)
 
         A_inv = np.linalg.inv(
             1 / sigma_n ** 2 * Phi @ Phi.T +
             np.diag(np.ones(Phi.shape[0])) / sigma_p ** 2)
-        self.wmean = 1 / sigma_n**2 * A_inv @ Phi @ self.ytrain
-        self.wcov = A_inv
+        self.coeff_mean = 1 / sigma_n**2 * A_inv @ Phi @ self.ytrain
+        self.coeff_cov = A_inv
 
         self.post_train()
         return Phi
@@ -43,8 +86,8 @@ class ChaospyLinReg(LinearRegression):
         Xpred = self.pre_predict(Xpred)
         Phi = self.transform(Xpred)
 
-        ymean = Phi.T @ self.wmean
-        ycov = Phi.T @ self.wcov @ Phi
+        ymean = Phi.T @ self.coeff_mean
+        ycov = Phi.T @ self.coeff_cov @ Phi
         # y_std = np.sqrt(np.diag(y_cov)) # TODO: include data variance
         return ymean, ycov
 
--- profit/sur/linreg/linear_regression.py
+++ profit/sur/linreg/linear_regression.py
@@ -4,13 +4,17 @@ Work in progress
 """
 
 import numpy as np
+import chaospy
 from profit.sur import Surrogate
+from profit.defaults import fit_linear_regression as defaults, fit as base_defaults
 
 
 class LinearRegression(Surrogate):
     """Base class for all Linear Regression models.
 
     Attributes:
+        trained (bool): Flag that indicates if the model is already trained and ready to make predictions.
+        # ToDo
 
     Parameters:
 
@@ -18,21 +22,19 @@ class LinearRegression(Surrogate):
 
     def __init__(self):
         super().__init__()
-        self.Mdim = None    # order of basis
-        self.Ndim = None    # number of data points
-        self.wmean = None   # MAP features
-        self.wcov = None    # covariance matrix of features
-        # TODO: add option to pass function transformer
+        self.coeff_mean = None
+        self.coeff_cov = None
 
-    def pre_train(self, X, y):
-        """
+    def pre_train(self, X, y, sigma_n, sigma_p):
+        """# ToDo
 
         """
-        self.Xtrain, self.ytrain = np.atleast_2d(X), np.atleast_2d(y).T
-        self.Ndim = self.Xtrain.shape[0]
+        self.Xtrain = X if len(X.shape) > 1 else X.reshape([-1, 1])
+        self.ytrain = X if len(y.shape) > 1 else y.reshape([-1, 1])
+
+        # ToDo: set attributes
+        self.sigma_n, self.sigma_p = sigma_n, sigma_p
         self.ndim = self.Xtrain.shape[-1]
-        if self.Mdim is None:
-            self.Mdim = self.ndim
 
     def post_train(self):
         self.trained = True
